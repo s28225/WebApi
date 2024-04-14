db19d15aa321f680286ff11e42d7ea77afe81f8a
s28225 <s28225@pjwstk.edu.pl>
2024-04-14 03:37:24 +0200

    ControllerAnimals and ObjectAnimals

---
+++ WebApplication1/WebApplication1/Controlers/AnimalsController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers;
+
+[Route("api/animals")]
+[ApiController]
+public class AnimalsController : ControllerBase
+{
+    private static readonly List<Animal> _animals = new List<Animal>()
+    {
+        new Animal ( 1,  "Thomas",  "Cat",  "White", 3.23 ),
+        new Animal ( 2,  "Ball",  "Dog",  "Brown", 37.45 ),
+        new Animal ( 3,  "Splinter",  "Rat",  "Black", 0.45 )
+
+    };
+        
+    [HttpGet]
+    public IActionResult GetAnimals()
+    {
+        return Ok(_animals);
+    }
+    
+    [HttpGet("{id:int}")]
+    public IActionResult GetAnimal(int id)
+    {
+        var animal = _animals.FirstOrDefault(s=> s.IdAnimal==id);
+        if (animal == null)
+        {
+            return NotFound("There no one with this id");
+        }
+
+        return Ok(animal);
+    }
+    [HttpPost]
+    public IActionResult AddAnimal(Animal animal)
+    {
+        _animals.Add(animal);
+        return StatusCode(StatusCodes.Status201Created);
+    }
+
+    [HttpPut("{id:int}")]
+    public IActionResult UpdateAnimal(int id, Animal animal)
+    {
+        var animalToEdit = _animals.FirstOrDefault(s => s.IdAnimal == id);
+
+        if (animalToEdit == null)
+        {
+            return NotFound("There no one with this id");
+        }
+
+        _animals.Remove(animalToEdit);
+        _animals.Add(animal);
+        return NoContent();
+    }
+
+    [HttpDelete("{id:int}")]
+    public IActionResult DeleteAnimal(int id)
+    {
+        var animalToDelete = _animals.FirstOrDefault(s => s.IdAnimal == id);
+        if (animalToDelete == null)
+        {
+            return NotFound("There is no such student");
+        }
+
+        _animals.Remove(animalToDelete);
+        return NoContent();
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Animal.cs
@@ -0,0 +1,19 @@
+﻿namespace WebApplication1.Models;
+
+public class Animal
+{
+    public int IdAnimal { get; set; }
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public string ColorCoat { get; set; }
+    public double Weight { get; set; }
+
+    public Animal(int idAnimal, string name, string category, string colorCoat, double weight)
+    {
+        this.IdAnimal = idAnimal;
+        this.Name = name;
+        this.Category = category;
+        this.ColorCoat = colorCoat;
+        this.Weight = weight;
+    }
+}
